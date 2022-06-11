73e95d55a37ac5e5bf7363ea236b3e1d4ceff2db
okatsuo <rafael.goncanna@gmail.com>
2022-06-11 16:01:03 -0300

    create usecases controllers

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,13 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { email, name } = request.body;
+      const newUser = this.createUserUseCase.execute({ email, name });
+      return response.status(201).json(newUser);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -5,8 +5,28 @@ import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
+  private getUserId = (request: Request): string => {
+    const { user_id } = request.headers;
+    return Array.isArray(user_id) ? user_id[0] : user_id;
+  };
+
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = this.getUserId(request);
+
+    if (!user_id) {
+      return response
+        .status(400)
+        .json({ error: "O header user_id é obrigatório." });
+    }
+
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({
+        user_id,
+      });
+      return response.json(allUsers);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
