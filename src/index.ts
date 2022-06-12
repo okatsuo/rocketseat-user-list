344d8c6f391fda47dbe535db7449b97d4b02716f
okatsuo <rafael.goncanna@gmail.com>
2022-06-11 21:42:48 -0300

    docs: make api docs with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import cors from "cors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJson from "./swagger.json";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use("/users", usersRoutes);
 
